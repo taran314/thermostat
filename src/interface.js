82a86d5b36952504846ec6cf1e4181b16650bae6
taran314 <tarandeep-nandhra@hotmail.co.uk>
2021-04-09 08:36:03 +0100

    Updated interface with temp colour and value update function

--- src/interface.js
+++ src/interface.js
@@ -9,32 +9,44 @@ $(document).ready(function() {
   //     $("#current_setting").text(thermostat.current_setting());
   //   });
   // });
+
+  function UpdateTemp() {
+    $("#current_setting").text(thermostat.current_setting());
+
+    if (thermostat.energyUsage() === "Low") {
+      $("#current_setting").css("color", "0000FF");
+    } else if (thermostat.energyUsage() === "Medium") {
+      $("#current_setting").css("color", "#000000");
+    } else {
+      $("#current_setting").css("color", "#FF0000");
+    }   
+  }
+
   $("#power_save_on").hide();
   $("#current_setting").text(thermostat.current_setting());
+
   $('#temperature_up').on('click', function() {
     thermostat.upTemp(1);
-    $("#current_setting").text(thermostat.current_setting());
+    UpdateTemp();
   });
   $('#temperature_reset').on('click', function() {
-      thermostat.resetTemp();
-    $("#current_setting").text(thermostat.current_setting());
-    // $(this).css("background-color", "blue");
+    thermostat.resetTemp();
+    UpdateTemp();
   });
   $( "#temperature_down" ).on( "click", function() {
-      thermostat.downTemp(1);
-    $("#current_setting").text(thermostat.current_setting());
-    // $("body").css("background-color","#99ffcc");
+    thermostat.downTemp(1);
+    UpdateTemp();
   });
   $("#power_save_on").on( "click", function() {
     thermostat.adjustPowerSave();
     $("#power_save_on").fadeOut();
     $("#power_save_off").fadeIn();
-    $("#current_setting").text(thermostat.current_setting());
+    UpdateTemp();
   });
   $("#power_save_off").on( "click", function() {
     thermostat.adjustPowerSave();
     $("#power_save_off").fadeOut();
     $("#power_save_on").fadeIn();
-    $("#current_setting").text(thermostat.current_setting());
+    UpdateTemp();
   });
 });
